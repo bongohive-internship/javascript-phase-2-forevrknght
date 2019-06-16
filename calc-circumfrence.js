54e86ab1a9b88803f63c49cb71bd258ca5c6dc22
John N0body <31930326+forevrknght@users.noreply.github.com>
2019-06-16 04:13:27 +0300

    Update calc-circumfrence.js

--- calc-circumfrence.js
+++ calc-circumfrence.js
@@ -1,1 +1,10 @@
 // calcCircumfrence function should be here
+function calcCircumfrence (radius){
+  
+  var c = 2 * Math.PI * radius;
+  window.alert('The Circumfrence is '+ c );
+}
+
+var rad = window.prompt('Whats your radius?');
+
+calcCircumfrence(rad);
