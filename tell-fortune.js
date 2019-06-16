5c64dad787f46bd88fa623dbb00873d43833b6a6
John N0body <31930326+forevrknght@users.noreply.github.com>
2019-06-16 04:03:35 +0300

    Update tell-fortune.js

--- tell-fortune.js
+++ tell-fortune.js
@@ -1,1 +1,16 @@
 // tellFortune function should be here
+function tellFortune(nc, pn, l, jt){
+  
+  var name = "me";
+  window.alert('You will be a ' + jt + ' in ' + l + ', and married to ' + pn + ' with '+ nc +' kids.');
+}
+                 
+  var nc = [ 2, 4, 6];
+  var pn = ["Julie", "Catherine", "Hannah"];
+  var jt = ["Lion Tamer", "Superhero", "Tap Dancer"];
+  var l = ["Zambia", "Congo", "Havana"];
+               
+ tellFortune(nc[Math.floor(Math.random() * 3)],pn[Math.floor(Math.random() * 3)], l[Math.floor(Math.random() * 3)], jt[Math.floor(Math.random() * 3)]);
+ tellFortune(nc[Math.floor(Math.random() * 3)],pn[Math.floor(Math.random() * 3)], l[Math.floor(Math.random() * 3)], jt[Math.floor(Math.random() * 3)]);
+  tellFortune(nc[Math.floor(Math.random() * 3)],pn[Math.floor(Math.random() * 3)], l[Math.floor(Math.random() * 3)], jt[Math.floor(Math.random() * 3)]);
+  
